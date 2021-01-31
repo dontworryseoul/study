ae593098caf804ad51fe34acd587f5800b44b528
jso <jso@student.42seoul.kr>
2021-01-31 19:32:24 +0900

    modified file

--- study/20210131/read_test.c
+++ study/20210131/read_test.c
@@ -6,7 +6,7 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 16:04:53 by jso               #+#    #+#             */
-/*   Updated: 2021/01/31 18:37:34 by jso              ###   ########.fr       */
+/*   Updated: 2021/01/31 19:18:35 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	read(fd, buf, 10);
 	printf("%s\n", buf);
+	close(fd);
 	return (0);
 }

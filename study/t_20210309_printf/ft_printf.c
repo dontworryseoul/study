134c6fcca909d463c726e508ea2a77d1d1a421de
jso <jso@jeonminhoui-MacBook-Pro.local>
2021-03-09 14:43:29 +0900

    add pritnf_rst func in ft_printf.c file

--- study/t_20210309_printf/ft_printf.c
+++ study/t_20210309_printf/ft_printf.c
@@ -1,8 +1,17 @@
 #include "ft_printf.h"
 
-void printf_rst(va_list ap, char *format)
+void printf_rst(va_list ap, char *fmt, t_flags *flags)
 {
-
+    while (*fmt)
+    {
+        if (*fmt != '%')
+        {
+            write(1, fmt, 1);
+            flags->ret++;
+        }
+        else
+            checker(ap, &(++fmt), flags);
+    }
 }
 
 int ft_printf(const char *format, ...)
@@ -12,7 +21,7 @@ int ft_printf(const char *format, ...)
 
     flags->ret = 0;
     va_start(ap, format);
-    printf_rst(ap, format);
+    printf_rst(ap, format, flags);
     va_end(ap);
     retrun (flags->ret);
 }

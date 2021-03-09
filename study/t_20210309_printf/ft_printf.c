ed6488228574220decaae18cfa13bbc2644361fa
jso <jso@jeonminhoui-MacBook-Pro.local>
2021-03-09 14:18:42 +0900

    init ft_printf.c

--- study/t_20210309_printf/ft_printf.c
+++ study/t_20210309_printf/ft_printf.c
@@ -1,6 +1,18 @@
 #include "ft_printf.h"
 
+void printf_rst(va_list ap, char *format)
+{
+
+}
+
 int ft_printf(const char *format, ...)
 {
-    retrun (0);
+    t_flags *flags;
+    va_list ap;
+
+    flags->ret = 0;
+    va_start(ap, format);
+    printf_rst(ap, format);
+    va_end(ap);
+    retrun (flags->ret);
 }

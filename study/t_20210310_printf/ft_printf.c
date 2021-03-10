da2ff960847efd0b8f8d8fa0a90fa8b4ce45cafa
jso <jso@student.42seoul.kr>
2021-03-11 05:44:02 +0900

    add ft_printf.c func, func name is init_flag

--- study/t_20210310_printf/ft_printf.c
+++ study/t_20210310_printf/ft_printf.c
@@ -1,7 +1,18 @@
 #include "ft_printf.h"
 
+static void init_flag(t_flags *flags)
+{
+	flags->left = 0;
+	flags->zero = 0;
+	flags->precision = 0;
+	flags->width = 0;
+	flags->ret = 0;
+	flags->type = 0;
+}
+
 static void check_fmt(va_list ap, const char **fmt, t_flags *flags)
 {
+	init_flag(flags);
 }
 
 static void print_rst(va_list ap, const char *fmt, t_flags *flags)
@@ -11,9 +22,11 @@ static void print_rst(va_list ap, const char *fmt, t_flags *flags)
 		if (*fmt == '%')
 			check_fmt(ap, &fmt, flags);
 		else
+		{
 			write(1, fmt, 1);
-		++fmt;
-		++flags->ret;
+			++flags->ret;
+		}
+	++fmt;
 	}
 }
 
@@ -22,8 +35,8 @@ int	ft_printf(const char *format, ...)
 	t_flags *flags;
 	va_list ap;
 
-	flags->ret = 0;
 	flags = malloc(sizeof(t_flags));
+	flags->ret = 0;
 	va_start(ap, format);
 	print_rst(ap, format, flags);
 	va_end(ap);

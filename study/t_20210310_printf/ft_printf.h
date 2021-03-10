4c28caeb11ac3dfc0eda1e29c5c9875023c6bfce
jso <jso@student.42seoul.kr>
2021-03-10 12:54:01 +0900

    ft_printf.h init

---
+++ study/t_20210310_printf/ft_printf.h
@@ -0,0 +1,17 @@
+#ifndef	FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+typedef	struct	s_flags
+{
+	int			ret;
+	int			type;
+	char		*string;
+}				t_flags;
+
+int				ft_printf(const char *format);
+
+#endif

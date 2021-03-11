d4399da07913ca685068efd71355e3418a0b3d31
jso <jso@student.42seoul.kr>
2021-03-11 20:25:52 +0900

    simple test code finished

---
+++ study/t_20210311_printf/main.c
@@ -0,0 +1,18 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int ret;
+	int i = 2;
+	
+	while (i--)
+	{
+		ret = ft_printf("Hello%cWorld", '+');
+		printf("\n");
+		printf("return value : %d", ret);
+		printf("\n");
+		printf("\n");
+	}
+	return (0):
+}

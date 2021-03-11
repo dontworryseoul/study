2b9e0e59f90ed2b44812f3a42ad53e4e474cf56a
jso <jso@student.42seoul.kr>
2021-03-11 20:36:58 +0900

    add ft_printf func, printf_rst

---
+++ study/t_20210311_printf/ft_printf.c
@@ -0,0 +1,29 @@
+#include "ft_printf.h"
+
+static void	printf_rst(va_list ap, const char *fmt, t_flags *flags)
+{
+	while (*fmt)
+	{
+		if (*fmt == '%')
+			printf_check_format(ap, &fmt, flags);
+		else
+		{
+			write(1, fmt, 1);
+			flags->ret++;
+		}
+	}
+}
+
+int			ft_printf(const char *format, ...)
+{
+	t_flags *flags;
+	va_list ap;
+
+	flags = malloc(sizeof(t_flags);
+	flags->ret = 0;
+	va_start(ap, format);
+	printf_rst(ap, format, flags);
+	va_end(ap);
+	free(flags);
+	return (flags->ret_value);
+}

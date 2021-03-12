48f817a96fb9e96b6259441d993977bcd283fda2
jso <jso@student.42seoul.kr>
2021-03-12 20:26:27 +0900

    separate file ft_printf_utils.c

---
+++ study/t_20210311_printf/ft_printf_utils.c
@@ -0,0 +1,18 @@
+#include "ft_printf.c"
+
+void printf_padding(int byte, t_flags *flags)
+{
+	char 	padding_char;
+	int		i;
+
+	padding_char = ' ';
+	i = 0;
+	flags->padding_byte = flags->width - byte;
+	if (flags->zero == 1 && flags->type == '%' && flags->left == 0)
+		padding_char = '0';
+	while (flags->padding_byte-- > 0)
+	{
+		write(1, &padding_char, 1);
+		flags->ret_value++;
+	}
+}

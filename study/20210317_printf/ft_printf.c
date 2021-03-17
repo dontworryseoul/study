344ddd472979326b0f162eaf3d30f511d67b436d
jso <jso@student.42seoul.kr>
2021-03-18 04:46:39 +0900

    rename from t_ to t

--- study/20210317_printf/ft_printf.c
+++ study/20210317_printf/ft_printf.c
@@ -6,16 +6,27 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:35:19 by jso               #+#    #+#             */
-/*   Updated: 2021/03/17 21:36:15 by jso              ###   ########.fr       */
+/*   Updated: 2021/03/17 22:43:46 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *fmt, ...)
+void	ft_printf_rst(va_list ap, const char *fmt, t_flags *flg)
+{
+	
+}
+
+int		ft_printf(const char *fmt, ...)
 {
 	va_list ap;
+	t_flags *flg;
 
+	flg = malloc(sizeof(t_flags));
 	g_ret_value = 0;
+	va_start(ap, fmt);
+	ft_printf_rst(ap, fmt, flg);
+	va_end(ap);
+	free(flg);
 	return(g_ret_value);
 }

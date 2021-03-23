77361ff47d9a2226c3f7c3e213883f294efed0bb
jso <jso@student.42seoul.kr>
2021-03-23 19:20:51 +0000

    test type s

--- study/t_20210322_printf/ft_printf_c_s_per.c
+++ study/t_20210322_printf/ft_printf_c_s_per.c
@@ -6,27 +6,11 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 08:39:55 by jso               #+#    #+#             */
-/*   Updated: 2021/03/23 16:44:34 by jso              ###   ########.fr       */
+/*   Updated: 2021/03/23 19:20:01 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_padding(int byte)
-{
-	char  padding_char;
-
-	if (g_flg.type == '%' && g_flg.zero && !g_flg.left)
-		padding_char = '0';
-	else
-		padding_char = ' ';
-	if (0 < (g_flg.width - byte))
-	{
-		g_flg.width = g_flg.width -byte;
-		while (g_flg.width--)
-			ft_printf_putchar(padding_char);
-	}
-}
 
 void	ft_print_c(int ch)
 {
@@ -70,12 +54,12 @@ void	ft_print_s(char *str)
 	if (str == NULL)
 		str = "(null)";
 	len = ft_strlen(str);
-	cp_len = len;
-	if (len > g_flg.prcs && -1 != g_flg.prcs)
+	if (len > g_flg.prcs && (-1 != g_flg.prcs))
 		len = g_flg.prcs;
+	cp_len = len;
 	if (g_flg.left)
 	{
-		while (len--)
+		while (len-- && str)
 		{
 			ft_printf_putchar(*str);
 			++str;
@@ -85,7 +69,7 @@ void	ft_print_s(char *str)
 	else
 	{
 		ft_padding(cp_len);
-		while (len--)
+		while (len-- && str)
 		{
 			ft_printf_putchar(*str);
 			++str;
--- study/t_20210322_printf/ft_printf_utils.c
+++ study/t_20210322_printf/ft_printf_utils.c
@@ -6,11 +6,27 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 12:36:55 by jso               #+#    #+#             */
-/*   Updated: 2021/03/23 16:34:14 by jso              ###   ########.fr       */
+/*   Updated: 2021/03/23 19:20:09 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_padding(int byte)
+{
+	char  padding_char;
+
+	if (g_flg.type == '%' && g_flg.zero && !g_flg.left)
+		padding_char = '0';
+	else
+		padding_char = ' ';
+	if (0 < (g_flg.width - byte))
+	{
+		g_flg.width = g_flg.width -byte;
+		while (g_flg.width--)
+			ft_printf_putchar(padding_char);
+	}
+}
 
 void	ft_printf_putchar(int c)
 {
@@ -20,7 +36,7 @@ void	ft_printf_putchar(int c)
 
 int		ft_strlen(const char *s)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
 	while (*s)

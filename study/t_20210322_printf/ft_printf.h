ee20b2044c6839bc172699f9f35dc685e69ff3a8
jso <jso@student.42seoul.kr>
2021-03-28 00:53:31 +0900

    norm test with header

--- study/t_20210322_printf/ft_printf.h
+++ study/t_20210322_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 07:26:10 by jso               #+#    #+#             */
-/*   Updated: 2021/03/28 00:40:44 by jso              ###   ########.fr       */
+/*   Updated: 2021/03/28 00:52:23 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # define HEX		16
 # define ONE_BYTE	1
 
-typedef struct s_flgs
+typedef struct	s_flgs
 {
 	int			left;
 	int			zero;
@@ -34,7 +34,7 @@ t_flgs			g_flg;
 
 int				ft_printf(const char *fmt, ...);
 
-/* 
+/*
 **				The funcs in the ft_find_fmt.c file below
 */
 
@@ -44,14 +44,14 @@ void			ft_check_flgs(const char **fmt);
 void			ft_check_width(va_list ap, const char **fmt);
 void			ft_check_prcs(va_list ap, const char **fmt);
 
-/* 
+/*
 **				The funcs in the ft_print_center.c file below
 */
 
 void			ft_check_type(const char **f);
 void			ft_print_types(va_list ap);
 
-/* 
+/*
 **				The funcs in the ft_print_c_s_per.c file below
 */
 
@@ -59,7 +59,7 @@ void			ft_print_c(int ch);
 void			ft_print_s(char *str);
 void			ft_print_percent(void);
 
-/* 
+/*
 **				The funcs in the ft_printf_utils.c file below
 */
 
@@ -67,7 +67,7 @@ void			ft_padding(int byte);
 void			ft_printf_putchar(int c);
 int				ft_strlen(const char *s);
 
-/* 
+/*
 **				The funcs in the ft_printf_d_u_x_p.c file below
 */
 
@@ -76,7 +76,7 @@ void			ft_print_x(unsigned int x_num);
 void			ft_print_p(long long p_num);
 void			ft_print_d(int n_num);
 
-/* 
+/*
 **				The funcs in the ft_print_comb.c file below
 */
 
@@ -85,7 +85,7 @@ void			ft_print_0_padding(int len);
 void			ft_print_recur(long long u_num, int n_base, char *base);
 void			ft_comb_u(long long u_num, int n_base, char *base);
 
-/* 
+/*
 **				The funcs in the ft_count_pad.c file below
 */
 

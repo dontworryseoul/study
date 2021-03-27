1330b722a597c579d5bfd4686bfbfc0eafd49ff1
jso <jso@student.42seoul.kr>
2021-03-28 00:43:00 +0900

    add files count_pad, print_comb

---
+++ study/t_20210322_printf/ft_count_pad.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_pad.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/28 00:25:57 by jso               #+#    #+#             */
+/*   Updated: 2021/03/28 00:26:15 by jso              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_divide_num(long long num, int base_num)
+{
+	int cnt;
+
+	cnt = 0;
+	if (num == 0)
+		return (1);
+	while (num)
+	{
+		num = num / base_num;
+		++cnt;
+	}
+	return (cnt);
+}
+
+int		ft_count_padding(long long num, int base_num)
+{
+	int	len;
+	int wid_0_len;
+
+	len = ft_divide_num(num, base_num);
+	if (0 < (g_flg.prcs - len))
+		len = g_flg.prcs;
+	wid_0_len = g_flg.width - len;
+	if (wid_0_len > 0 && g_flg.zero && g_flg.left == 0 && g_flg.prcs < 0)
+		len = g_flg.width;
+	if (!g_flg.prcs && !num)
+		--len;
+	return (len);
+}
+
+int		ft_count_d_padding(long long num, int base_num)
+{
+	int	len;
+	int cp_len;
+	int wid_0_len;
+
+	len = ft_divide_num(num, base_num);
+	cp_len = len;
+	if (num < 0)
+		++cp_len;
+	wid_0_len = g_flg.width - cp_len;
+	while (g_flg.prcs > len++)
+		++cp_len;
+	while (wid_0_len > 0 && g_flg.zero && !g_flg.left && g_flg.prcs < 0)
+	{
+		--wid_0_len;
+		++cp_len;
+	}
+	if (!g_flg.prcs && !num)
+		--cp_len;
+	return (cp_len);
+}
---
+++ study/t_20210322_printf/ft_print_comb.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/28 00:23:37 by jso               #+#    #+#             */
+/*   Updated: 2021/03/28 00:24:19 by jso              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_print_0_prcs(int len)
+{
+	while (g_flg.prcs > len++)
+		ft_printf_putchar('0');
+}
+
+void	ft_print_0_padding(int len)
+{
+	int cp_len;
+
+	cp_len = len;
+	if (g_flg.left == 0 && g_flg.zero == 1 && g_flg.prcs < 0)
+	{
+		while (cp_len-- > 0)
+			ft_printf_putchar('0');
+	}
+}
+
+void	ft_print_recur(long long u_num, int n_base, char *base)
+{
+	if (u_num >= (unsigned int)n_base)
+		ft_print_recur(u_num / (unsigned int)n_base, n_base, base);
+	ft_printf_putchar(base[u_num % (unsigned int)n_base]);
+}
+
+void	ft_comb_u(long long u_num, int n_base, char *base)
+{
+	int		len;
+	int		cp_len;
+
+	if (base[16] == 'p')
+	{
+		ft_printf_putchar('0');
+		ft_printf_putchar('x');
+	}
+	if ((base[10] == 'd') && u_num < 0)
+		ft_printf_putchar('-');
+	len = ft_divide_num(u_num, n_base);
+	ft_print_0_prcs(len);
+	cp_len = g_flg.width - len;
+	if (u_num < 0)
+	{
+		--cp_len;
+		u_num *= -1;
+	}
+	ft_print_0_padding(cp_len);
+	if (!(g_flg.prcs == 0 && u_num == 0))
+		ft_print_recur(u_num, n_base, base);
+}

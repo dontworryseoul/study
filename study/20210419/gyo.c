5016a10b7547c21c879c3770820f516d7fae60ab
jso <jso@student.42seoul.kr>
2021-04-19 10:35:46 +0900

    test gyo.c

--- study/20210419/gyo.c
+++ study/20210419/gyo.c
@@ -1,8 +1,40 @@
 #include <unistd.h>
 
-int main(void)
+void new_line(void)
 {
-	write(1, "Hello", 5);
 	write(1, "\n", 1);
-	return (0);
+}
+
+void gyo(char *s1, char *s2)
+{
+	int asci[256];
+	int i, j;
+
+	i = 0;
+	while (i < 256)
+		asci[i++] = 0;
+	i = 0;
+	while (s1[i])
+	{
+		j = 0;
+		while (s2[j] && s1[i] != s2[j])
+			j++;
+		if (s1[i] == s2[j] && asci[(int)s1[i]] == 0)
+		{
+			asci[(int)s1[i]] = 1;
+			write(1, s1 + i, 1);
+		}
+		i++;
+	}
+}
+
+int main(int ac, char *av[])
+{
+	if (ac != 3)
+	{
+		new_line();
+		return (0);
+	}
+	gyo(av[1], av[2]);
+	new_line();
 }
